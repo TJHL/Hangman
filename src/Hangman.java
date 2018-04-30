b2520dd970208860b7c3cea7beb523a811eceb2b
TJHL <league@WTS14-4.attlocal.net>
2018-04-29 17:24:53 -0700

    Make Popup work, and start 2nd word

--- src/Hangman.java
+++ src/Hangman.java
@@ -1,5 +1,3 @@
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.io.BufferedReader;
@@ -14,121 +12,133 @@ import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
-public class Hangman implements KeyListener{
+public class Hangman implements KeyListener {
 	JFrame frame;
 	JPanel panel;
 	JLabel label;
-	String display="";
-	ArrayList <String> dox;
-	ArrayList <String> hangmanReadyWords;
-	String tempForFileReader= "";
+	String display = "";
+	ArrayList<String> dox;
+	ArrayList<String> hangmanReadyWords;
+	String tempForFileReader = "";
 	int randomInt;
-	boolean needNewWord= true;
-	int currentWord=0;
-	String currentWordUp="";
-	String blanks="";
-	String toBeDisplayed ="";
+	boolean needNewWord = true;
+	int currentWord = 0;
+	String currentWordUp = "";
+	String blanks = "";
+	String toBeDisplayed = "";
 	char charTyped;
-	String temporatyToBeUpdated ="";
-	
-	
-	
-public static void main(String[] args) {
-	Hangman a = new Hangman();
-	
-}
-	Hangman(){
-	dox= new ArrayList <String>();
-	hangmanReadyWords= new ArrayList <String>();
-	findWords();
-	randomHangmanWords();
-	panel = new JPanel();
-	frame = new JFrame();
-	label = new JLabel(display);
-	displayWord();
-	frame.add(panel);
-	panel.add(label);
-	frame.addKeyListener(this);
-	frame.setSize(500, 500);
-	frame.setLocation(700, 230);
-	frame.setVisible(true);
-	
-	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-}
-	
-	void findWords(){
-			try {
-				BufferedReader br = new BufferedReader(new FileReader("src/dictionary.txt"));
-				
+	String temporatyToBeUpdated = "";
+
+	public static void main(String[] args) {
+		Hangman a = new Hangman();
+
+	}
+
+	Hangman() {
+		dox = new ArrayList<String>();
+		hangmanReadyWords = new ArrayList<String>();
+		findWords();
+		randomHangmanWords();
+		panel = new JPanel();
+		frame = new JFrame();
+		label = new JLabel(display);
+		displayWord();
+		frame.add(panel);
+		panel.add(label);
+		frame.addKeyListener(this);
+		frame.setSize(500, 500);
+		frame.setLocation(700, 230);
+		frame.setVisible(true);
+
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+	void findWords() {
+		try {
+			BufferedReader br = new BufferedReader(new FileReader("src/dictionary.txt"));
+
+			tempForFileReader = br.readLine();
+			while (tempForFileReader != null) {
+				dox.add(tempForFileReader);
 				tempForFileReader = br.readLine();
-				while(tempForFileReader != null){
-					dox.add(tempForFileReader);
-					tempForFileReader = br.readLine();
-				}
-				
-				br.close();
-			} catch (FileNotFoundException e1) {
-				
-				e1.printStackTrace();
-			} catch (IOException e) {
-				
-				e.printStackTrace();
 			}
+
+			br.close();
+		} catch (FileNotFoundException e1) {
+
+			e1.printStackTrace();
+		} catch (IOException e) {
+
+			e.printStackTrace();
 		}
-	
+	}
+
 	void randomHangmanWords() {
 		Random r = new Random();
 		for (int i = 0; i < dox.size(); i++) {
-		randomInt= r.nextInt(dox.size());
+			randomInt = r.nextInt(dox.size());
 			hangmanReadyWords.add(dox.get(randomInt));
-			
+
 		}
 	}
-	
 
-		void displayWord () {
-			if(needNewWord== true) {
-				
-				for (int i = 0; i < hangmanReadyWords.get(currentWord).length(); i++) {
-					blanks += "_";
-				}
-				toBeDisplayed= toBeDisplayed +blanks;
-				currentWordUp=hangmanReadyWords.get(currentWord);
-				updateDisplay();
-				
-				System.out.println(hangmanReadyWords.get(currentWord));
-		
-				needNewWord= false;	
+	void displayWord() {
+		if (needNewWord == true) {
+
+			for (int i = 0; i < hangmanReadyWords.get(currentWord).length(); i++) {
+				blanks += "_";
 			}
-			
-		}
-		void updateWord () {
-			temporatyToBeUpdated="";
-			for(int i=0;i<hangmanReadyWords.get(currentWord).length();i++) {
-				if(currentWordUp.charAt(i)==charTyped) {
-					System.out.println(charTyped);
-					temporatyToBeUpdated+=currentWordUp.charAt(i);
-				}else {
-					temporatyToBeUpdated+= blanks.charAt(i);
-			}
-				}
-			toBeDisplayed=temporatyToBeUpdated;
-			
+			toBeDisplayed = toBeDisplayed + blanks;
+			currentWordUp = hangmanReadyWords.get(currentWord);
 			updateDisplay();
-		}
-		void updateDisplay(){
-			display="Guess a letter \n"+toBeDisplayed+" You have "+"INSERT THINGS"+" attemps left";
-			label.setText(display);
+
+			System.out.println(hangmanReadyWords.get(currentWord));
+
+			needNewWord = false;
 		}
 
-		
+	}
 
-		public void keyTyped(KeyEvent e) {
-			charTyped=e.getKeyChar();
-			updateWord();
-			
+	void updateWord() {
+		temporatyToBeUpdated = "";
+		for (int i = 0; i < hangmanReadyWords.get(currentWord).length(); i++) {
+			if (toBeDisplayed.charAt(i) == currentWordUp.charAt(i)) {
+
+				temporatyToBeUpdated += currentWordUp.charAt(i);
+			} else if (currentWordUp.charAt(i) == charTyped) {
+
+				temporatyToBeUpdated += currentWordUp.charAt(i);
+			} else {
+				temporatyToBeUpdated += blanks.charAt(i);
+			}
 		}
-	
-		public void keyPressed(KeyEvent e) {}public void keyReleased(KeyEvent e) {}
-	
+		toBeDisplayed = temporatyToBeUpdated;
+
+		updateDisplay();
+		finishedWord();
+	}
+
+	void updateDisplay() {
+		display = "Guess a letter \n" + toBeDisplayed + " You have " + "INSERT THINGS" + " attemps left";
+		label.setText(display);
+	}
+
+	void finishedWord() {
+		if (toBeDisplayed == currentWordUp) {
+			JOptionPane.showMessageDialog(null, "Good Job! You got the word correct!!!");
+		}
+	}
+
+	public void keyTyped(KeyEvent e) {
+		charTyped = e.getKeyChar();
+		updateWord();
+
+	}
+
+	public void keyPressed(KeyEvent e) {
+	}
+
+	public void keyReleased(KeyEvent e) {
+	}
+
 }
